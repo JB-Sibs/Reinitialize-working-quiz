ee0f35b1d3e4654d816d4db0684a8016b6ea7f04
Janel Ullrich B. Sibal <jsibal1@student.hau.edu.ph>
2024-10-04 13:41:14 +0800

    Working forgot password

--- Canvas_campus/settings.py
+++ Canvas_campus/settings.py
@@ -86,6 +86,13 @@ DATABASES = {
     }
 }
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'canvascampusca@gmail.com'
+EMAIL_HOST_PASSWORD = 'rqxpdzmdxiomqugy'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
